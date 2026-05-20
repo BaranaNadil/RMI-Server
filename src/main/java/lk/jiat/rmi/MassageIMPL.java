b2aceab0fca6eaed7c9699a5d0b842877f1ee348
BaranaNadil <161505829+BaranaNadil@users.noreply.github.com>
2026-05-20 11:56:11 +0530

    Extends UnicastRemoteObject to MassageIMPL class for run server continuously for Enabled remote method invocation for clients to access MassageIMPL

--- src/main/java/lk/jiat/rmi/MassageIMPL.java
+++ src/main/java/lk/jiat/rmi/MassageIMPL.java
@@ -1,8 +1,14 @@
 package lk.jiat.rmi;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class MassageIMPL implements Massage, Remote {
+public class MassageIMPL extends UnicastRemoteObject implements Massage, Remote {
+
+    protected MassageIMPL() throws RemoteException {
+    }
+
     @Override
     public void hello() {
         System.out.println("Server: hello....");
